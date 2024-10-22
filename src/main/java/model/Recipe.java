e9b944d9408852d6abbaf85ec8fc2c3eae9eeef5
Parth Surve <146193646+ParthSurve07@users.noreply.github.com>
2024-10-22 14:50:15 +0530

    Add files via upload

--- src/main/java/model/Recipe.java
+++ src/main/java/model/Recipe.java
@@ -3,44 +3,34 @@ package model;
 import java.util.ArrayList;
 
 public class Recipe {
-    private String recipeName;
-    private ArrayList<Ingredient> ingredients;
-    private String instructions;
+    private String name; // Field to store the recipe name
+    private String instructions; // Field to store cooking instructions
+    private ArrayList<Ingredient> ingredients; // List to hold ingredients
 
-    public Recipe(String recipeName, String instructions) {
-        this.recipeName = recipeName;
+    public Recipe(String name, String instructions) {
+        this.name = name;
         this.instructions = instructions;
         this.ingredients = new ArrayList<>();
     }
 
-    public String getRecipeName() {
-        return recipeName;
-    }
-    public ArrayList<Ingredient> getIngredients() {
-        return ingredients;
-    }
-    public String getInstructions() {
-        return instructions;
+    public String getName() {
+        return name;
     }
 
-    public void setRecipeName(String recipeName) {
-        this.recipeName = recipeName;
-    }
-    public void setIngredients(ArrayList<Ingredient> ingredients) {
-        this.ingredients = ingredients;
-    }
-    public void setInstructions() {
-        this.instructions = instructions;
+    public String getInstructions() {
+        return instructions;
     }
 
     public void addIngredient(Ingredient ingredient) {
         ingredients.add(ingredient);
     }
-    public void removeIngredient(Ingredient ingredient) {
-        ingredients.remove(ingredient);
+
+    public ArrayList<Ingredient> getIngredients() {
+        return ingredients;
     }
 
+    @Override
     public String toString() {
-        return "Recipe: " + recipeName + "\nIngredients: " + ingredients.toString() + "\nInstructions: " + instructions;
+        return "Recipe: " + name + "\nInstructions: " + instructions + "\nIngredients: " + ingredients.toString();
     }
 }
